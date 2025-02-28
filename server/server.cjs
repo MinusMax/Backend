fce865a900ce6a420c9b2cfa4007f9fd1acfb589
MinusMax <107465828+MinusMax@users.noreply.github.com>
2025-02-28 13:24:56 +0700

    Add files via upload

---
+++ server/server.cjs
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'vnpschool',
+    password: 'mixky21365',
+    port: 5432,
+});
+
+app.post('/apply', async (req, res) => {
+    const { first_name, last_name, grade, address } = req.body;
+    if (!first_name || !last_name || !grade || !address) {
+        return res.status(400).json({ message: 'All fields are required.' });
+    }
+    
+    try {
+        const insertQuery =
+            `INSERT INTO students (first_name, last_name, grade, address)
+            VALUES ($1, $2, $3 , $4)`;
+
+        const values = [
+            first_name,
+            last_name,
+            grade,
+            address,
+        ];
+
+        const result = await pool.query(insertQuery, values);
+
+        console.log('Student apply:', result.rows[0]);
+
+        res.status(201).json({ message: 'Student apply successfully!' });
+    } catch (error) {
+        console.error('Error apply user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.get('/search', async (req, res) => {
+    try {
+        const { name } = req.query;
+        if (!name) {
+            return res.status(400).json({ error: "Name is required" });
+        }
+
+        const result = await pool.query(
+            "SELECT * FROM students WHERE first_name ILIKE $1 OR last_name ILIKE $1", 
+            [`%${name}%`]
+        );
+
+        res.json(result.rows);
+    } catch (err) {
+        console.error("Database error:", err.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
